131f14976a61cca998df5eb57e82494c0bc62811
Mahi Uddin <rasel@m2sys.com>
2020-01-16 20:01:31 +0600

    Face extractor console app
    Added face extractor console app.  Given image and you will be gets only face images

---
+++ face.image.extractor/face.image.extractor.consoleapp/FaceExtractor.cs
@@ -0,0 +1,101 @@
+﻿using DlibDotNet;
+using DlibDotNet.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace face.image.extractor.consoleapp
+{
+    /// <summary>
+    /// Face extractor
+    /// </summary>
+    public class FaceExtractor
+    {
+        private readonly ShapePredictor _ShapePredictor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shapePredictorFilePath"></param>
+        public FaceExtractor(string shapePredictorFilePath)
+        {
+            this._ShapePredictor = ShapePredictor.Deserialize(shapePredictorFilePath);
+        }
+
+        /// <summary>
+        /// Returns all images
+        /// </summary>
+        /// <param name="imagePath"></param>
+        /// <returns></returns>
+        public List<Image> GetImage(string imagePath)
+        {
+            List<Image> images = new List<Image>();
+
+            try
+            {
+                using (var faceDetector = Dlib.GetFrontalFaceDetector())
+                using (var img = Dlib.LoadImage<RgbPixel>(imagePath))
+                {
+                    Dlib.PyramidUp(img);
+
+                    var dets = faceDetector.Operator(img);
+
+                    var shapes = new List<FullObjectDetection>();
+                    foreach (var rect in dets)
+                    {
+                        var shape = this._ShapePredictor.Detect(img, rect);
+                        if (shape.Parts <= 2)
+                            continue;
+                        shapes.Add(shape);
+                    }
+
+                    if (shapes.Any())
+                    {
+                        /*
+                         * if you want to draw line on images then you may uncomment this commeting blocks
+                        var lines = Dlib.RenderFaceDetections(shapes);
+                        
+                        foreach (var line in lines)
+                            Dlib.DrawLine(img, line.Point1, line.Point2, new RgbPixel
+                            {
+                                Green = 255
+                            });
+                       
+                        foreach (var l in lines)
+                            l.Dispose();
+                        */
+
+                        var chipLocations = Dlib.GetFaceChipDetails(shapes);
+
+                        using (var faceChips = Dlib.ExtractImageChips<RgbPixel>(img, chipLocations))
+                        {
+                            Image image = null;
+                            //Iterate face chips and add image one by one if multiple images existed
+                            foreach (var face in faceChips)
+                            {
+                                image = face.ToBitmap();
+                                images.Add(image);
+                            }
+                        }
+
+                        foreach (var c in chipLocations)
+                            c.Dispose();
+                    }
+
+                    foreach (var s in shapes)
+                        s.Dispose();
+                }
+                return images;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
+
---
+++ face.image.extractor/face.image.extractor.consoleapp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace face.image.extractor.consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Starting to extract image...");
+                //Image file path
+                string imageFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "5d4dba24b22c9.jpg");
+                string shapePredictorFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "shape_predictor_68_face_landmarks.dat");
+                //Initiate Extract class
+                FaceExtractor extractImage = new FaceExtractor(shapePredictorFilePath);
+                //Call GetImage method by given your image file path. You should be get list of images if face image exist on that given image
+                List<Image> images = extractImage.GetImage(imageFilePath);
+                Console.WriteLine("Face extraction done!");
+                Console.WriteLine("Number of face extracted: " + images.Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Occured: " + ex.Message);
+                if (ex.InnerException != null) Console.WriteLine("InnerException: " + ex.Message);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
